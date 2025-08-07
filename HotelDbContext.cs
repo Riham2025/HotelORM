25fb773e4d8c1aeb63a91c6f2f97ce4e78914413
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-07 21:52:25 +0400

    This class represents the database context for the hotel management system

---
+++ HotelDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelORM
+{
+    public class HotelDbContext //
+    {
+        // This class represents the database context for the hotel management system.
+        // It will be used to interact with the database, such as querying and saving data.
+        // In a real application, this would typically inherit from DbContext in Entity Framework.
+        // For now, this class is empty and serves as a placeholder for future implementation.
+    
+    }
+}
