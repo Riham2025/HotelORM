e84db540b91e4bbaf22a5f9120384ccd133499a9
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-07 22:11:57 +0400

    This class is responsible for managing room-related operations in the hotel management system

---
+++ Services/RoomService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelORM.Models;
+using HotelORM.Repository;
+
+namespace HotelORM.Services
+{
+    public class RoomService // This class is responsible for managing room-related operations in the hotel management system.
+    {
+        private readonly RoomRepository _roomRepo;
+
+        public RoomService(RoomRepository roomRepo)
+        {
+            _roomRepo = roomRepo;
+        }
+
+        public void CreateRoom(string number)
+        {
+            Room room = new Room { RoomNumber = number, IsAvailable = true };
+            _roomRepo.AddRoom(room);
+        }
+
+        public void SetRoomAvailable(int roomId, bool isAvailable)
+        {
+            _roomRepo.UpdateRoomAvailability(roomId, isAvailable);
+        }
+
+        public List<Room> ListRooms() => _roomRepo.GetAllRooms();
+    }
+
+}
