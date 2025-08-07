c7a9b42d74e2efc300f37e19e1844914889f9924
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-07 21:59:29 +0400

    This class is responsible for managing room data in the hotel management system

--- Repository/RoomRepository.cs
+++ Repository/RoomRepository.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelORM.Models;
 
 namespace HotelORM.Repository
 {
-    public class RoomRepository
+    public class RoomRepository 
+                                // This class is responsible for managing room data in the hotel management system.
+                                // It provides methods to add, retrieve, and update room information in the database.
+                                // The class interacts with the HotelDbContext to perform database operations.
+                                // In a real application, this would typically implement an interface for better abstraction.
     {
+        private readonly HotelDbContext _context;
+
+        public RoomRepository(HotelDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+            _context.SaveChanges();
+        }
+
+        public List<Room> GetAllRooms() => _context.Rooms.ToList();
+
+        public void UpdateRoomAvailability(int roomId, bool available)
+        {
+            var room = _context.Rooms.FirstOrDefault(r => r.RoomId == roomId);
+            if (room != null)
+            {
+                room.IsAvailable = available;
+                _context.SaveChanges();
+            }
+        }
     }
+
 }
