4510e34ebac0fb035254bf4d7328f2e93231c372
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-07 22:19:08 +0400

    This creates an instance of the RoomRepository, which will be used to interact with the room data in the database

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace HotelORM
+﻿using HotelORM.Repository;
+using HotelORM.Services;
+
+namespace HotelORM
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using HotelDbContext context = new HotelDbContext(); 
+            context.Database.EnsureCreated(); 
+
+            RoomRepository roomRepo = new RoomRepository(context); // This creates an instance of the RoomRepository, which will be used to interact with the room data in the database.
+            RoomService roomService = new RoomService(roomRepo);
+
+            roomService.CreateRoom("101");
+            roomService.CreateRoom("102");
+
+            foreach (var room in roomService.ListRooms())
+            {
+                Console.WriteLine($"Room: {room.RoomNumber} - Available: {room.IsAvailable}");
+            }
         }
+
     }
 }
